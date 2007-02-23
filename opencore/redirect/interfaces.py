a1cfe14e5369578aafe432d769c18924178d4454
Whit Morriss <d.w.morriss@gmail.com>
2007-02-23 22:02:39 +0000

    IRedirectInfo
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/trunk@3123 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/redirect/interfaces.py
+++ opencore/redirect/interfaces.py
@@ -1,18 +1,18 @@
 from zope.interface.common.mapping import IReadMapping, IWriteMapping
-from zope.interface import Interface
+from zope.interface import Interface, Attribute
+
+try:
+    from zope.annotation.interfaces import IAnnotatable
+except ImportError:
+    from zope.app.annotation.interfaces import IAnnotatable
 
 
-class IRedirectable(Interface):
+class IRedirected(IAnnotatable):
     """An object that submits to selective redirection traversal"""
 
 
-class IRedirectMapping(IReadMapping, IWriteMapping):
-    """ a mapping object for paths """
+class IRedirectInfo(IReadMapping, IWriteMapping):
+    """Redirect annotation BTree bag"""
+    url = Attribute('base url for redirection')
+    parent = Attribute('physical path to a parent object')
 
-    def __getitem__(key):
-        """Get a value for a key(a path)
-
-        A KeyError is raised if there is no value for the key.
-
-        interpolates path into a qualified address
-        """
