ca1ff9738b2a3e30d458e141cf73f47980b70630
Whit Morriss <d.w.morriss@gmail.com>
2007-03-07 16:25:06 +0000

    stubbing out how this might work
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/trunk@3370 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/redirect/subproject_listing.py
+++ opencore/redirect/subproject_listing.py
@@ -1,15 +1,28 @@
 from Products.OpenPlans.browser.projectlisting import ProjectListingView 
 from memojito import memoizedproperty
 
+from opencore.redirect import get_redirect_info
+from Products.CMFCore.utils import getToolByName
 
 class SubProjectListingView(ProjectListingView): 
     """
     """
 
-    #@memoizedproperty
-    #def allprojects(self): 
-    #    return however_we_figure_this_out_given_an_IRedirected()
+    @memoizedproperty
+    def redirect_info(self): 
+        return get_redirect_info(self.context)
 
+    @property
+    def project_paths(self):
+        return list(self.redirect_info.values())
+
+    @memoizedproperty
+    def portal_catalog(self):
+        return getToolByName(self, 'portal_catalog')
+        
+    @memoizedproperty
+    def allprojects(self): 
+        return self.portal_catalog(path=self.project_paths, sort_on='sortable_title')
 
     @property
     def alpha(self): 
