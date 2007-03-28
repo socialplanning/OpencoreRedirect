81787582e0a81d27396037c3b6f0d322e911eff9
Whit Morriss <d.w.morriss@gmail.com>
2007-03-28 15:30:46 +0000

    move directive interface to be canonical default host interface(since hackyform will also use this)
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/branches/whit-bughunt0703@3845 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/redirect/interfaces.py
+++ opencore/redirect/interfaces.py
@@ -1,5 +1,6 @@
 from zope.interface.common.mapping import IReadMapping, IWriteMapping
 from zope.interface import Interface, Attribute, implements
+from zope.schema import TextLine
 
 try:
     from zope.annotation.interfaces import IAnnotatable
@@ -19,6 +20,19 @@ class IRedirectInfo(IReadMapping, IWriteMapping):
     """Redirect annotation BTree bag"""
     url = Attribute('base url for redirection')
     parent = Attribute('physical path to a parent object')
+
+
+class IDefaultHost(Interface):
+    """utility declaratively representing the default host for an
+    instance('opencore.redirect.default_host')"""
+    host = TextLine(
+        title=u"Default Host", 
+        description=u"The hostname to default to")
+
+    path = TextLine(
+        title=u"Default Path Prefix", 
+        description=u"The default path to prepend to the object",
+        )
 
 
 class ITestObject(Interface):
