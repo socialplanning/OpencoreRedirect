0aae094b1b115ba25712289f7d8e32308da40403
Whit Morriss <d.w.morriss@gmail.com>
2007-03-28 15:08:30 +0000

    whitespace
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/trunk@3840 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/redirect/interfaces.py
+++ opencore/redirect/interfaces.py
@@ -10,8 +10,10 @@ except ImportError:
 class IRedirected(IAnnotatable):
     """An object that submits to selective redirection traversal"""
 
+
 class INotRedirected(Interface): 
     """An object that is ignored by the redirector under all circumstances"""
+
 
 class IRedirectInfo(IReadMapping, IWriteMapping):
     """Redirect annotation BTree bag"""
