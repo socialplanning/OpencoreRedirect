7337d2bf72901da67b56e26fc4b67d239ed84ed8
Whit Morriss <d.w.morriss@gmail.com>
2007-04-06 22:31:19 +0000

    abstract some base class, add code for dh view(working for non persistent)
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/branches/whit-bughunt0703@4088 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/redirect/management.py
+++ opencore/redirect/management.py
@@ -1,27 +1,76 @@
-from zope.interface import Interface, implements
-from zope.component import adapts
-from zope.formlib import form
-from plone.app.form.validators import null_validator
-from interfaces import IRedirectSetup
+from OFS.Folder import Folder
+from interfaces import IRedirectSetup, IDefaultHost
+from memojito import memoizedproperty
 from opencore import redirect 
 from opencore.redirect import LOG
-import logging
-from memojito import memoizedproperty
-
 from opencore.redirect.classproperty import property as classproperty
-from opencore import redirect 
+from plone.app.form.validators import null_validator
 from plone.fieldsets.form import FieldsetsEditForm
-from OFS.Folder import Folder
+from zope.app.component.interfaces import ISite, IPossibleSite
+from zope.component import adapts, getUtility
+from zope.formlib import form
+from zope.interface import Interface, implements
+import logging
+from five.intid.site import addUtility
 
-class RedirectConfigSchemaAdapter(object):
-    adapts(Folder)
-    implements(IRedirectSetup)
-    
+class BaseAdapter(object):
     def __init__(self, context):
         self.context = context
 ##         ptool = getUtility(IPropertiesTool)
 ##         self.encoding = ptool.getProperty('default_charset', None)
 
+class BaseForm(FieldsetsEditForm): 
+    @form.action('Save changes')
+    def handle_edit_action(self, action, data):
+        if form.applyChanges(self.context, self.form_fields, data,
+                             self.adapters):
+            self.status = "Changes saved."
+        else:
+            self.status = "No changes made."
+
+    @form.action(u'Cancel', validator=null_validator)
+    def handle_cancel_action(self, action, data):
+        IStatusMessage(self.request).addStatusMessage("Changes canceled.", type="info")
+        url = getMultiAdapter((self.context, self.request), name='absolute_url')()
+        self.request.response.redirect(url)
+        return
+
+
+class RedirectSetupForm(BaseForm):
+    form_fields = form.FormFields(IRedirectSetup)
+    label = 'Activate Redirection' 
+
+class DefaultHostForm(BaseForm):
+    form_fields = form.FormFields(IDefaultHost)
+    label = 'Set Default Host' 
+
+class DefaultHostSchemaAdapter(BaseAdapter):
+    adapts(IPossibleSite)
+    implements(IDefaultHost)
+
+    @memoizedproperty
+    def dh(self):
+        return getUtility(IDefaultHost)
+
+    class path(classproperty):
+        def fget(self):
+            return self.dh.path
+            
+        def fset(self, val):
+            self.dh.path = val
+
+    class host(classproperty):
+        def fget(self):
+            return self.dh.host
+            
+        def fset(self, val):
+            self.dh.host = val
+
+
+class RedirectConfigSchemaAdapter(BaseAdapter):
+    adapts(Folder)
+    implements(IRedirectSetup)
+    
     @memoizedproperty
     def info(self):
         return redirect.get_info(self.context)
@@ -52,30 +101,6 @@ class RedirectConfigSchemaAdapter(object):
 
 
 
-class RedirectSetupForm(FieldsetsEditForm): 
-    form_fields = form.FormFields(IRedirectSetup)
 
-    label = 'Activate Redirection' 
 
-    @form.action('Save changes')
-    def handle_edit_action(self, action, data):
-        if form.applyChanges(self.context, self.form_fields, data,
-                             self.adapters):
-            self.status = "Redirection changes saved."
-        else:
-            self.status = "No changes made."
 
-    @form.action(u'Cancel', validator=null_validator)
-    def handle_cancel_action(self, action, data):
-        IStatusMessage(self.request).addStatusMessage("Changes canceled.", type="info")
-        url = getMultiAdapter((self.context, self.request), name='absolute_url')()
-        self.request.response.redirect(url)
-        return
-
-##     @form.action('Activate')
-##     def handle_activate_action(self, action, data): 
-##         redirect_url = data.get('redirect_url')
-        
-##         logging.getLogger(LOG).info("Setting redirect url %s on %s" % (redirect_url, self.context))
-##         redirect.activate(self.context, url=redirect_url)
-##         self.status = "Activated Redirection"
