6b2137759f6e613a62e84b4db2edf093ddf2e3ff
Rob Miller <rob@kalistra.com>
2007-04-10 17:51:42 +0000

    * putting dep links back into setup.py
    git-svn-id: https://svn.socialplanning.org/svn/OpencoreRedirect/branches/whit-bughunt0703@4115 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import sys, os
 
 version = '0.2'
 
-deplinks = ["https://svn.openplans.org/svn/OpencoreRedirect/branches/whit-bughunt0703/dependency_links.html",
+deplinks = ["https://svn.plone.org/svn/collective/five.intid/trunk#egg=five.intid-dev",
+            "http://svn.plone.org/svn/plone/plone.fieldsets/trunk#egg=plone.fieldsets",
+            "http://svn.plone.org/svn/plone/plone.app.form/branches/plone-3.0#egg=plone.app.form",
             ]
 
 setup(name='OpencoreRedirect',
